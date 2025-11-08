4347501979f5c2d17698976282eb0e9607a9591f
paikalsrl <113494785+paikalsrl@users.noreply.github.com>
2025-11-08 15:29:34 +0900

    [update] 예산 기준 세분화

--- src/main/java/com/example/capstone/plan/entity/TravelDay.java
+++ src/main/java/com/example/capstone/plan/entity/TravelDay.java
@@ -6,7 +6,7 @@ import lombok.*;
 import java.util.List;
 
 @Entity
-@Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
--- src/main/java/com/example/capstone/plan/entity/TravelPlace.java
+++ src/main/java/com/example/capstone/plan/entity/TravelPlace.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
